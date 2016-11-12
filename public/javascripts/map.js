bfb48789028a32599ce4a44da5242d4c82b3feaa
robinliu106 <robinliu@bu.edu>
2016-11-11 20:17:06 -0500

    cleaned up code

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,21 +1,19 @@
-//initialize array of marked locations
-var markedLocations = [];
-
 
 function initialize() {
+    var markedLocations = [];
+    labelIndex = 0; //reset labelIndex in addMarker
+
     var boston = { lat:42.342132, lng: -71.103023 };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: boston
     });
 
-    // This event listener calls addMarker() when the map is clicked.
-    google.maps.event.addListener(map, 'click', function(event) {
-        addMarker(event.latLng, map);
-        //push marker to array markedLocations
-        markedLocations.push( [ event.latLng.lat() , event.latLng.lng() ] );
-        //find distance between marked point and various locations
-        getDistance( [event.latLng.lat() , event.latLng.lng()] );
+    //Create a marker on the map
+    google.maps.event.addListener(map, 'click', function(marker) {
+        addMarker(marker.latLng, map); //add marker to map
+        markedLocations.push(marker.latLng); //save marker
+        getDistance(marker.latLng); //get distance
     });
 
     //map these values to index page
@@ -23,11 +21,18 @@ function initialize() {
     var dest_address = document.getElementById('dest_address');
     var distance_in_meters = document.getElementById('distance_in_meters');
 
+    //set the default text
+    origin_address.innerHTML = 'Marker Address: ';
+    dest_address.innerHTML = 'Closest Hospital Address: ';
+    distance_in_meters.innerHTML = 'Distance: ';
+
+
 }
 
 // Each marker is labeled with a single alphabetical character.
 var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var labelIndex = 0;
+
 // Adds a marker to the map.
 function addMarker(location, map) {
     // Add the marker at the clicked location, and add the next-available label
@@ -37,54 +42,32 @@ function addMarker(location, map) {
         label: labels[labelIndex++ % labels.length],
         map: map
     });
-
 }
-
-//delete markers button
-function removeMarkers() {
-    markedLocations = []; //reset markedLocations to null
-    labelIndex = 0; //reset labelIndex to 0
-
-    //wipe addresses
-    origin_address.innerHTML = 'Marker Address: ';
-    dest_address.innerHTML = 'Closest Hospital Address: ';
-    distance_in_meters.innerHTML = 'Distance: ';
-
-    initialize(); //reload the map with no markers
-}
-
-
 
 //get distance between two (long,lat) points when a marker is clicked
-//documentation
 //https://developers.google.com/maps/documentation/javascript/examples/distance-matrix
 function getDistance(origin) {
-
-    var origin = new google.maps.LatLng(origin[0],origin[1]);
-
-    //test
-    var hospitalsList = [ [42.3631542,-71.0710221] , [42.3457464,-71.1032591] , [42.3457464,-71.1032591] ];
-    var hospitals = generatePoints(hospitalsList);
-    //alert(hospitals);
-
     var service = new google.maps.DistanceMatrixService();
 
+    //test data
+    var hospitalsList = [ [42.3631542,-71.0710221] , [42.3457464,-71.1032591] , [42.3457464,-71.1032591] ];
+    var hospitals = generatePoints(hospitalsList);
+
+    //get distance
     service.getDistanceMatrix({
         origins: [origin],
         destinations: hospitals,
         travelMode: google.maps.TravelMode.DRIVING,
-        avoidHighways: false,
-        avoidTolls: false,
         unitSystem: google.maps.UnitSystem.IMPERIAL
-    },
-    callback);
+    }, callback);
 
+    //parse response
     function callback(response, status) {
         if (status == "OK") {
 
             //calculate minimum distance
             var minDistance = Infinity;
-            var counter = 0
+            var counter = 0 //track which one is the min.
             for (var i = 0; i < response.rows[0].elements.length; i++) {
                 var currentDistance = response.rows[0].elements[i].distance.value;
                 if (currentDistance < minDistance) {
@@ -97,19 +80,17 @@ function getDistance(origin) {
             origin_address.innerHTML = 'Marker Address: ' + response.originAddresses;
             dest_address.innerHTML = 'Closest Hospital Address: ' + response.destinationAddresses[counter];
             distance_in_meters.innerHTML = 'Distance: ' + minDistance + ' meters';
+
         } else {
             alert("Error: " + status);
         }
-
     }
-
 }
 
 function generatePoints(array) {
     var result = [];
     for (var i = 0 ; i < array.length; i++) {
-        var point = new google.maps.LatLng(array[i][0],array[i][1]);
-        result.push( point );
+        result.push( new google.maps.LatLng(array[i][0],array[i][1]) );
     }
     return result;
 }
