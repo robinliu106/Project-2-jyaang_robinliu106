151b49f40d15023478eab2b8da7441e20c1c44ab
robinliu106 <robinliu@bu.edu>
2016-11-13 16:46:40 -0500

    working on getdistance, not working yet

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,7 +1,24 @@
+//get hospital data in format : [ name,coord[] ]
+function fetchHospitals(){
+    var hospitals = [];
+    $.getJSON('https://data.cityofboston.gov/api/views/46f7-2snz/rows.json?accessType=DOWNLOAD',{ },
+    function(response) {
 
-google.maps.event.addDomListener(window, 'load', initialize);
+        for (var i = 0; i < response.data.length; i++) {
+            hospitals.push([ response.data[i][8] , [response.data[i][14][1],response.data[i][14][2]] ] );
+        }
+
+    });
+
+    return hospitals;
+
+}
+
 
 function initialize() {
+
+    var hospitals = fetchHospitals();
+    //console.log(hospitals);
     labelIndex = 0; //reset labelIndex in addMarker
 
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -12,7 +29,8 @@ function initialize() {
     //Create a marker on the map
     google.maps.event.addListener(map, 'click', function(marker) {
         addMarker(marker.latLng, map); //add marker to map
-        getDistance(marker.latLng); //get distance
+        getDistance(marker.latLng,hospitals); //get distance for hospital
+
     });
 
     //set the default text
@@ -21,12 +39,15 @@ function initialize() {
     document.getElementById('distance').innerHTML = 'Distance: ';
 
     //heat map
+    /*
     var heatmap = new google.maps.visualization.HeatmapLayer({
         data: generatePoints( [[42.3631542,-71.0710221],[42.3457464,-71.1032591],[42.3457464,-71.1032591]] ),
         map: map
     });
+    */
 
-    fetchHospitals();
+
+
 }
 
 var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // Each marker is labeled with a letter
@@ -45,16 +66,12 @@ function addMarker(location, map) {
 
 //get distance between two (long,lat) points when a marker is clicked
 //https://developers.google.com/maps/documentation/javascript/examples/distance-matrix
-function getDistance(origin) {
+function getDistance(origin, type) {
     var service = new google.maps.DistanceMatrixService();
 
-    //test data
-    var hospitals = generatePoints( [[42.3631542,-71.0710221],[42.3457464,-71.1032591],[42.3457464,-71.1032591]] );
-    var crimes = [];
-    var schools = [];
-    var dayCamps = [];
+    var hospitals = generatePoints(type);
+    console.log(hospitals);
 
-    //get distance JSON
     service.getDistanceMatrix({
         origins: [origin],
         destinations: hospitals,
@@ -67,7 +84,7 @@ function getDistance(origin) {
         if (status == "OK") {
 
             //view the json response in console
-            //console.log(JSON.stringify(response,null,4));
+            console.log(JSON.stringify(response,null,4));
 
             //calculate minimum distance
             var minDistance = Infinity;
@@ -85,61 +102,31 @@ function getDistance(origin) {
             document.getElementById('dest_address').innerHTML = 'Closest Hospital Address: ' + response.destinationAddresses[counter];
             document.getElementById('distance').innerHTML = 'Distance: ' + minDistance + ' meters';
 
+        } else {
+            console.log('didnt work');
         }
     }
+
 }
 
 //generate google maps points
 function generatePoints(array) {
+    //console.log(array);
+    //var test = [[42.3631542,-71.0710221],[42.3457464,-71.1032591],[42.3457464,-71.1032591]];
     var result = [];
+
     for (var i = 0 ; i < array.length; i++) {
-        result.push( new google.maps.LatLng(array[i][0],array[i][1]) );
+        //result.push(new google.maps.LatLng(test[i][0],test[i][1]));
+        //console.log(new google.maps.LatLng(parseFloat(array[i][1][0]),parseFloat(array[i][1][1])));
+        result.push( new google.maps.LatLng(parseFloat(array[i][1][0]),parseFloat(array[i][1][1])));
     }
+
+    //console.log(result);
     return result;
-}
-
-//get hospital data in format : [ name,coord[] ]
-function fetchHospitals(){
-    var hospitals = [];
-    $.getJSON('https://data.cityofboston.gov/api/views/46f7-2snz/rows.json?accessType=DOWNLOAD',{ },
-    function(response) {
-        console.log(response.data);
-
-        for (var i = 0; i < response.data.length; i++) {
-            hospitals.push([ response.data[i][8] , [response.data[i][14][1],response.data[i][14][2]] ] );
-        }
-        //console.log(hospitals);
-
-        return hospitals;
-    });
 
 }
 
 
 
 
-/*
-url = "https://data.cityofboston.gov/api/views/46f7-2snz/rows.json?accessType=DOWNLOAD"
-JSON_response = urllib.request.urlopen(url).read().decode("utf-8")
-JSON_object = json.loads(JSON_response) #deserializes a str containing a JSON object to a list
-
-hospitalData = JSON_object['data']
-
-test = hospitalData[0][14][0]
-test = test[1:-1]
-test = test.split(',')
-#print(test[1])
-test = str(test[1]).split(':')
-
-#print(test[1])
-
-a = []
-for hospital in hospitalData:
-    city = hospital[14][0]
-    city = city[1:-1].split(',')
-    city = str(city[1]).split(':')[1]
-    city = city.strip("\"")
-    a.append({"hospitalName" : hospital[8] , "city" : city , "coord" : hospital[-1][1:3] })
-p = json.dumps(a)
-
-*/
+google.maps.event.addDomListener(window, 'load', initialize);
